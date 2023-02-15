ce49efd0f00cad9ff93a6bb13c6accd18be5ad00
Documenter.jl <documenter@juliadocs.github.io>
2023-02-15 02:00:30 +0000

    build based on 7a5e675

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CognitiveModel","category":"page"},{"location":"#CognitiveModel","page":"Home","title":"CognitiveModel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CognitiveModel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CognitiveModel]","category":"page"}]
+}
