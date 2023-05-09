bf5d3e08ca700800ce1a9687c4bd75c73a3e5e03
Song921012 <aidishage@gmail.com>
2023-05-09 09:52:29 +0800

    update

--- 202305epineural.jl
+++ 202305epineural.jl
@@ -11,20 +11,20 @@ rng = Random.default_rng()
 
 data = DataFrame(CSV.File("./output/datasmoothing.csv"))
 choosentime = range(87, 150)
-choosencolumn = [3,5] # vaccined individuals, cases
+choosencolumn = [3, 5] # vaccined individuals, cases
 trainingdata = Array(data[choosentime, choosencolumn])'
-datascale=Array(data[choosentime, 2:end])'
+datascale = Array(data[choosentime, 2:end])'
 
 # set up neural differential equation models
-N=38250000.0f0
-σ1=0.19
-γ1=0.1
-hi0=Float32(trainingdata[1,1])
-hv0=Float32(trainingdata[2,1])
-v0=Float32(trainingdata[2,1])
-e0=Float32(datascale[1,1]/σ1)
-i0=Float32(datascale[1,1]/γ1)
-u0 = [N,v0,e0,i0,hi0,hv0]
+N = 38250000.0f0
+σ1 = 0.19
+γ1 = 0.1
+hi0 = Float32(trainingdata[1, 1])
+hv0 = Float32(trainingdata[2, 1])
+v0 = Float32(trainingdata[2, 1])
+e0 = Float32(datascale[1, 1] / σ1)
+i0 = Float32(datascale[1, 1] / γ1)
+u0 = [N, v0, e0, i0, hi0, hv0]
 datasize = 64
 tspan = (0.0f0, 64.0f0)
 tsteps = range(tspan[1], tspan[2], length=datasize)
@@ -34,17 +34,17 @@ ann = Lux.Chain(Lux.Dense(1, 32, relu), Lux.Dense(32, 32, tanh), Lux.Dense(32, 2
 p, st = Lux.setup(rng, ann)
 ann([0.1], p, st)[1]
 function SVEIR_nn(du, u, p, t)
-    S,V,E,I,HI,HV = u
-    N=38250000.0f0
-    ϵ=0.8f0
-    σ1=0.19f0
-    γ1=0.1f0
-    du[1] = - min(1, abs(ann([t], p, st)[1][1])) * I * S/N- abs(ann([t], p, st)[1][2]) * S
-    du[2] = abs(ann([t], p, st)[1][2]) * S - (1.0f0-ϵ)* min(1, abs(ann([t], p, st)[1][1])) * I * V/N
-    du[3] = min(1, abs(ann([t], p, st)[1][1])) * I * S/N + (1.0f0-ϵ)* min(1, abs(ann([t], p, st)[1][1])) * I * V/N - σ1 * E
-    du[4] = σ1 * E -γ1 * I
-    du[5] = min(1, abs(ann([t], p, st)[1][1])) * I * S/N + (1.0f0-ϵ)* min(1, abs(ann([t], p, st)[1][1])) * I * V/N
-    du[6]= abs(ann([t], p, st)[1][2]) * S
+    S, V, E, I, HI, HV = u
+    N = 38250000.0f0
+    ϵ = 0.8f0
+    σ1 = 0.19f0
+    γ1 = 0.1f0
+    du[1] = -min(1, abs(ann([t], p, st)[1][1])) * I * S / N - abs(ann([t], p, st)[1][2]) * S
+    du[2] = abs(ann([t], p, st)[1][2]) * S - (1.0f0 - ϵ) * min(1, abs(ann([t], p, st)[1][1])) * I * V / N
+    du[3] = min(1, abs(ann([t], p, st)[1][1])) * I * S / N + (1.0f0 - ϵ) * min(1, abs(ann([t], p, st)[1][1])) * I * V / N - σ1 * E
+    du[4] = σ1 * E - γ1 * I
+    du[5] = min(1, abs(ann([t], p, st)[1][1])) * I * S / N + (1.0f0 - ϵ) * min(1, abs(ann([t], p, st)[1][1])) * I * V / N
+    du[6] = abs(ann([t], p, st)[1][2]) * S
 end
 prob_neuralode = ODEProblem(SVEIR_nn, u0, tspan, ComponentArray(p))
 
@@ -56,12 +56,12 @@ function predict_neuralode(θ)
     Array(solve(prob, Tsit5(), saveat=tsteps))
 end
 
-predict_neuralode(p)[5:6,:]
-size(predict_neuralode(p)[5:6,:]) == size(trainingdata)
+predict_neuralode(p)[5:6, :]
+size(predict_neuralode(p)[5:6, :]) == size(trainingdata)
 # loss function and callbacks
 
 function loss_neuralode(p)
-    pred = predict_neuralode(p)[5:6,:]
+    pred = predict_neuralode(p)[5:6, :]
     loss = sum(abs2, log.(trainingdata) .- log.(pred))
     return loss, pred
 end
@@ -93,7 +93,7 @@ optf = Optimization.OptimizationFunction((x, p) -> loss_neuralode(x), adtype)
 optprob = Optimization.OptimizationProblem(optf, pinit)
 
 result_neuralode = Optimization.solve(optprob,
-OptimizationOptimisers.ADAM(0.05),
+    OptimizationOptimisers.ADAM(0.05),
     callback=callback,
     maxiters=300)
 
@@ -107,7 +107,7 @@ result_neuralode2 = Optimization.solve(optprob2,
 optprob2 = remake(optprob, u0=result_neuralode2.u)
 
 result_neuralode2 = Optimization.solve(optprob2,
-Optimisers.ADAM(0.001),
+    Optimisers.ADAM(0.001),
     maxiters=300,
     callback=callback,
     allow_f_increases=false)
@@ -138,8 +138,8 @@ callback(pfinal, loss_neuralode(pfinal)...; doplot=true)
 ##
 # beta(t), nu(t) function
 
-β(t)=min(1, abs(ann([t], pfinal, st)[1][1]))
-ν(t)=abs(ann([t],pfinal,st)[1][2])
+β(t) = min(1, abs(ann([t], pfinal, st)[1][1]))
+ν(t) = abs(ann([t], pfinal, st)[1][2])
 
 plot(tsteps, β.(tsteps))
 plot(tsteps, ν.(tsteps))
@@ -148,12 +148,12 @@ plot(tsteps, ν.(tsteps))
 using BSON: @save
 @save "./output/annepi.bson" ann
 @save "./output/annepipfinal.bson" pfinal
-pred = predict_neuralode(pfinal)[5:6,:]
+pred = predict_neuralode(pfinal)[5:6, :]
 plt = scatter(tsteps, trainingdata[1, :], label="Accumulated cases")
 plot!(plt, tsteps, pred[1, :], label="Predicted accumulated cases")
 display(plot(plt))
 savefig("./output/annepicase.png")
-plt=scatter(tsteps, trainingdata[2, :], label="Accumulated vaccinated individuals")
+plt = scatter(tsteps, trainingdata[2, :], label="Accumulated vaccinated individuals")
 #scatter!(tsteps, trainingdata[3, :], label="Minter data")
 #scatter!(tsteps, trainingdata[4, :], label="Cinter data")
 
@@ -169,8 +169,8 @@ neuralepi[!, "Case"] = trainingdata[1, :]
 neuralepi[!, "Vaccine"] = trainingdata[2, :]
 neuralepi[!, "PredCase"] = pred[1, :]
 neuralepi[!, "PredVaccine"] = pred[2, :]
-neuralepi[!,"beta"]=β.(tsteps)
-neuralepi[!,"nu"]=ν.(tsteps)
+neuralepi[!, "beta"] = β.(tsteps)
+neuralepi[!, "nu"] = ν.(tsteps)
 CSV.write("./output/neuralepi.csv", neuralepi)
 
 ##
